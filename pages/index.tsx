a13e5085f82628382bf7fa5bbfe7b7f033c0eea0
elodiebellec <66627649+elodiebellec@users.noreply.github.com>
2020-08-25 14:44:18 +0200

    ajout paragraphe

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ import { usePlugin } from 'tinacms'
 import { GetStaticProps } from 'next'
 
 export default function Home({ file, preview }) {
+
   const formOptions = {
     label: 'Home Page',
     fields: [{ name: 'title', component: 'text' }],
@@ -39,39 +40,16 @@ export default function Home({ file, preview }) {
           {data.title}
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <p className="article">
+          {/*
+           ** Render the article from `home.json`
+           */}
+          {data.article}
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        
       </main>
 
       <footer>
